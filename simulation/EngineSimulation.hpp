63feb62ec965587404e4c2b05a820f86f3feec85
Marius Vinturis <mvinturis@gmail.com>
2019-10-09 00:29:34 +0300

    Update EngineSimulation.hpp

--- simulation/EngineSimulation.hpp
+++ simulation/EngineSimulation.hpp
@@ -81,31 +81,36 @@ protected:
 		case ACCELERATE_SECOND_GEAR:
 			// on shifting gears, we loose a bit of speed and the engine rotation changes
 			restartParallelAnimation(30, 26, 6.1, 2.4, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "2");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "2");
 			restartParallelAnimation(26, 60, 2.4, 5.6, ACCELERATION_INTERVAL);
 			break;
 		case ACCELERATE_THIRD_GEAR:
 			// on shifting gears, we loose a bit of speed and the engine rotation changes
 			restartParallelAnimation(60, 56, 5.6, 2.3, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "3");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "3");
 			restartParallelAnimation(56, 100, 2.3, 5.1, ACCELERATION_INTERVAL);
 			break;
 		case ACCELERATE_FOURTH_GEAR:
 			// on shifting gears, we loose a bit of speed and the engine rotation changes
 			restartParallelAnimation(100, 96, 5.1, 2.2, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "4");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "4");
 			restartParallelAnimation(96, 140, 2.2, 6.2, ACCELERATION_INTERVAL);
 			break;
 		case ACCELERATE_FIFTH_GEAR:
 			// on shifting gears, we loose a bit of speed and the engine rotation changes
 			restartParallelAnimation(140, 136, 6.2, 2.3, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "5");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "5");
 			restartParallelAnimation(136, 170, 2.3, 4.8, ACCELERATION_INTERVAL);
 			break;
 		case ACCELERATE_SIXTH_GEAR:
 			// on shifting gears, we loose a bit of speed and the engine rotation changes
 			restartParallelAnimation(170, 169, 4.8, 2.7, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "6");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "6");
 			restartParallelAnimation(169, 210, 2.7, 5.2, ACCELERATION_INTERVAL);
 			break;
 		case BREAK_SIXTH_GEAR:
@@ -115,27 +120,32 @@ protected:
 			break;
 		case BREAK_FIFTH_GEAR:
 			restartParallelAnimation(170, 169, 2.5, 4.9, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "5");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "5");
 			restartParallelAnimation(169, 140, 4.9, 2.3, ACCELERATION_INTERVAL);
 			break;
 		case BREAK_FOURTH_GEAR:
 			restartParallelAnimation(140, 139, 2.3, 4.2, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "4");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "4");
 			restartParallelAnimation(139, 100, 4.2, 2.3, ACCELERATION_INTERVAL);
 			break;
 		case BREAK_THIRD_GEAR:
 			restartParallelAnimation(100, 99, 2.3, 4.1, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "3");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "3");
 			restartParallelAnimation(99, 60, 4.1, 2.2, ACCELERATION_INTERVAL);
 			break;
 		case BREAK_SECOND_GEAR:
 			restartParallelAnimation(60, 59, 2.2, 3.2, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "2");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "2");
 			restartParallelAnimation(59, 30, 3.2, 1.9, ACCELERATION_INTERVAL);
 			break;
 		case BREAK_FIRST_GEAR:
 			restartParallelAnimation(30, 29, 1.9, 2.8, GEAR_SHIFT_INTERVAL, true);
-			notifyValueChanged(GEAR_SHIFT, "1");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "1");
 			restartParallelAnimation(29, 0, 2.8, 1, ACCELERATION_INTERVAL);
 			break;
 		default:
@@ -172,7 +182,8 @@ protected:
 		((int&)step)++;
 		if (step >= FINISH)
 		{
-			notifyValueChanged(GEAR_SHIFT, "0");
+			if (notifyValueChanged)
+				notifyValueChanged(GEAR_SHIFT, "0");
 			stop_flag = true;
 			return;
 		}
